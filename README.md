6de5785c7b2086513417222c2f7ae5825f28a8c4
HyeokJin96 <gurwls7590@gmail.com>
2023-01-31 10:13:20 +0900

    Puzzle object drag ok

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Tangram
 Copy mobile tangram game
 
-2023-01-30 / V0.1.0 / Setup Tangram
+2023-01-30 / V0.1.0 / Setup Tangram game progect
+2023-01-30 / V0.2.0 / Setup Tangram Title Scene
+2023-03-31 / v0.2.1 / Puzzle object drag ok
--- Tangram/Assets/01.UnityProject/Scripts/Runtime/02.PlayScene/PuzzlePlayPart.cs
+++ Tangram/Assets/01.UnityProject/Scripts/Runtime/02.PlayScene/PuzzlePlayPart.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PuzzlePlayPart : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerMoveHandler
+public class PuzzlePlayPart : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     private bool isClicked = false;
+    private RectTransform objRect = default;
+    private PuzzleInitZone puzzleInitZone = default;
 
     // Start is called before the first frame update
     void Start()
     {
         isClicked = false;
+        objRect = gameObject.GetRect();
+        puzzleInitZone = transform.parent.gameObject.GetComponentMust<PuzzleInitZone>();
     }   //  Start()
 
     // Update is called once per frame
@@ -23,29 +27,23 @@ public class PuzzlePlayPart : MonoBehaviour, IPointerDownHandler, IPointerUpHand
     public void OnPointerDown(PointerEventData eventData)
     {
         isClicked = true;
-
-        //  DEBUG:
-        //GFunc.Log($"{gameObject.name}을 선택했다.");
     }   //  OnPointerDown()
 
     //! 마우스 버튼에서 손을 뗐을 때 동작하는 함수
     public void OnPointerUp(PointerEventData eventData)
     {
         isClicked = false;
-
-        //  DEBUG:
-        //GFunc.Log($"{gameObject.name}을 선택 해제했다.");
     }   //  OnPointerUp()
 
     //! 마우스를 드래그 중일 때 동작하는 함수
-    public void OnPointerMove(PointerEventData eventData)
+    public void OnDrag(PointerEventData eventData)
     {
         if (isClicked == true)
         {
-            gameObject.SetLocalPos(eventData.position.x, eventData.position.y, 0f);
-            GFunc.Log($"마우스의 포지션을 눈으로 확인 : {eventData.position.x}, {eventData.position.y}");
+            //  절대 좌표를 설정하는 로직 -> 상대적 움직임을 현재 포지션에 더하는 로직으로 수정 eventData.delta
+            gameObject.AddAnchoredPos(eventData.delta / puzzleInitZone.parentCanvas.scaleFactor);
         }   //  if : 현재 오브젝트를 선택한 경우
-    }   //  OnPointerMove()
+    }   //  OnDrag()
 
 
 
